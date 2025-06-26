fc7c83205cc6b51bec163aae6263a0e3d8f83696
Jay Paul <Jaypaul276@gmail.com>
2025-06-27 00:29:21 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const users = {
+  "Jay": "jay123",
+  "Milan": "milan123",
+  "Alice": "alice123"
+};
+
+const socketToUser = {};
+const userToSocket = {};
+
+io.on("connection", (socket) => {
+  socket.on("login", ({ username, password }) => {
+    if (users[username] === password) {
+      socketToUser[socket.id] = username;
+      userToSocket[username] = socket.id;
+      socket.emit("login-success", username);
+      io.emit("user-list", Object.keys(userToSocket));
+    } else {
+      socket.emit("login-failed");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const username = socketToUser[socket.id];
+    if (username) {
+      delete socketToUser[socket.id];
+      delete userToSocket[username];
+      io.emit("user-list", Object.keys(userToSocket));
+    }
+  });
+
+  socket.on("offer", ({ to, offer }) => {
+    const target = userToSocket[to];
+    if (target) {
+      io.to(target).emit("incoming-call", { from: socketToUser[socket.id], offer });
+    }
+  });
+
+  socket.on("answer", ({ to, answer }) => {
+    const target = userToSocket[to];
+    if (target) {
+      io.to(target).emit("answer", { answer });
+    }
+  });
+
+  socket.on("candidate", ({ to, candidate }) => {
+    const target = userToSocket[to];
+    if (target) {
+      io.to(target).emit("candidate", { candidate });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
